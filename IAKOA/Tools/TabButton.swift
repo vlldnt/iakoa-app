f8270c781b1846298b2f0e24a290c6089fcea98d
vlldnt <vieilledent.adrien@gmail.com>
2025-07-16 10:01:24 +0200

    feat: add TabButton component for customizable tab navigation

---
+++ IAKOA/Tools/TabButton.swift
@@ -0,0 +1,16 @@
+struct TabButton: View {
+        let title: String
+        let isSelected: Bool
+        let action: () -> Void
+
+        var body: some View {
+            Button(action: action) {
+                Text(title)
+                    .fontWeight(isSelected ? .bold : .medium)
+                    .foregroundColor(isSelected ? .white : .black)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 44)
+            }
+            .contentShape(Rectangle())
+        }
+    }
